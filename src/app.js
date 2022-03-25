4e6bf663fa59b7f815914f2ac95def760e161420
Taffarel Xavier <taffarel_deus@hotmail.com>
2022-03-25 12:25:12 -0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import Exemplo from './components/Exemplo.vue';
+new Vue({
+    el: '#app',
+    components: { Exemplo }
+});
---
+++ webpack.mix.js
@@ -0,0 +1,2 @@
+let mix = require('laravel-mix');
+mix.js('src/app.js', 'public/js').vue();
